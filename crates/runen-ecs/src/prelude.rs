b3280fbf86ce07e96949a69de952d9d99c105420
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-24 18:27:43 +0200

    style(ecs): format gameplay prelude

--- crates/runen-ecs/src/prelude.rs
+++ crates/runen-ecs/src/prelude.rs
@@ -1,6 +1,6 @@
 pub use crate::{
-    Added, BatchCommands, Bundle, Changed, Commands, Component, CyclePolicy, Directed, Entity, Query,
-    Relation, RelationConstraints, RelationError, Relations, RelationsMut, Removed, RemovedQuery, Res,
-    ResMut, Resource, Runtime, ScheduleLabel, SelfRelation, SourceCardinality, Symmetric,
-    SystemConfigExt, SystemMobilityExt, SystemSet, With, Without, World,
+    Added, BatchCommands, Bundle, Changed, Commands, Component, CyclePolicy, Directed, Entity,
+    Query, Relation, RelationConstraints, RelationError, Relations, RelationsMut, Removed,
+    RemovedQuery, Res, ResMut, Resource, Runtime, ScheduleLabel, SelfRelation, SourceCardinality,
+    Symmetric, SystemConfigExt, SystemMobilityExt, SystemSet, With, Without, World,
 };
